11d64e82338fce7f5145b5b44b63ac7b81cd76a1
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 08:58:47 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main >
-      <header classname={styles.header}>
+      <header className={styles.header}>
         <div>
           <h1>
             <span>
-          Hello!<span classname={styles.yellowText}>.</span>
+          Hello!<span className={styles.yellowText}>.</span>
             </span>
             <br />
             <span>
@@ -19,6 +19,36 @@ export default function Home() {
           </h1>
         </div>
       </header>
+      <section className={styles.infoSection}>
+        <img 
+          src="/imagenlab2023.png"
+          alt="imagen"
+          className={styles.image}
+
+        />
+        <div className={styles.infoContainer}>
+          <span className={styles.title + " " + styles.yellowText}>Alicia</span>
+          <br/>
+          <span className={styles.title}>Toledo</span>
+     
+        <div className={styles.list}>
+          <ul>
+            <li>
+              <span className={styles.grayText}>Age: </span> 17
+              </li>
+            <li>
+             <span className={styles.grayText}>Nationality:</span>  Guatemalan
+              </li>
+            <li>
+              <span className={styles.grayText}>Skill set: </span>  idk
+              </li>
+            <li>
+             <span className={styles.grayText}>Languages: </span>  English, Spanish and French
+              </li>
+          </ul>
+        </div>
+        </div>
+      </section>
     </main>
   )
 }
