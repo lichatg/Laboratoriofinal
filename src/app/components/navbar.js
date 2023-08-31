8d34041d9ddbfd9feffcba39ac1470b0acfe142c
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 10:15:09 -0600

    Animation NavBar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,26 +1,36 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from './navbar.module.css';
 
 function Navbar() {
-    const [selectedItem, setSelectedItem]=useState('Inicio');
+    const [selectedItem, setSelectedItem] = useState('Inicio');
 
-    const handleItemClick=(item)=> {
+    const handleItemClick = (item, id) => {
+        setSelectedItem(item);
+        scrollToSection(id);
 
     };
 
-    return(
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+
+        }
+    }
+
+    return (
         <nav className={styles.Navbar}>
-         <div className={styles.NavbarItem + ` ${selectedItem === 'Inicio' ? styles.selected: ''}`}
-         onClick={() => handleItemClick('Inicio')}
-         >
-            <a href="#header">Inicio</a>
-         </div>
-         <div className={styles.NavbarItem + ` ${selectedItem === 'Skills' ? styles.selected:''}`}
-         onClick={() => handleItemClick('Skills')}
-         >
-            <a href="#info">Skills</a>
-         </div>
+            <div className={styles.NavbarItem + ` ${selectedItem === 'Inicio' ? styles.selected : ''}`}
+                onClick={() => handleItemClick('Inicio', 'header')}
+            >
+                <a href="#header">Inicio</a>
+            </div>
+            <div className={styles.NavbarItem + ` ${selectedItem === 'Skills' ? styles.selected : ''}`}
+                onClick={() => handleItemClick('Skills', 'info')}
+            >
+                <a href="#info">Skills</a>
+            </div>
         </nav>
     );
 }
